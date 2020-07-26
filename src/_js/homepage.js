4bfe1db384876315a6cedd2c02d368017d822802
Vince Falconi <vince.falconi@gmail.com>
2020-07-26 13:58:21 -0500

    oh look endless tinkering... on the intro message

--- src/_js/homepage.js
+++ src/_js/homepage.js
@@ -1,5 +1,4 @@
 (() => {
-	const descriptionSpans = document.querySelectorAll('[data-description]');
 	const list = [
 		'tattoo-collector',
 		'weightlifter',
@@ -8,10 +7,16 @@
 		'rescuer of Princess Zelda',
 		'lover of horror',
 	];
+	const descriptionSpans = document.querySelectorAll('[data-description]');
 	const descriptionSet = new Set();
 	let loop = 0;
 
-	while (descriptionSet.size <= 2) {
+	// sets cannot contain duplicates, so this loop will run until
+	// descriptionSet has enough items in it to update the
+	// placeholders on the page. this also scales up or down based
+	// on the number of placeholders, without neededing to change this
+	// loop. <3
+	while (descriptionSet.size <= descriptionSpans.length - 1) {
 		const itemIndex = Math.floor(Math.random() * list.length);
 		descriptionSet.add(list[itemIndex]);
 	}
