3074a69d50a5fae7aa473922cc5d3e7fbff353f5
Vince Falconi <vince.falconi@gmail.com>
2020-09-12 12:50:08 -0500

    remove unneeded console.logs

--- src/_js/head.js
+++ src/_js/head.js
@@ -1,9 +1,6 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(registration => {
-		console.log('Service worker registration succeeded');
-	}).catch(error => {
-		console.log('Service worker registration failed');
-	});
-} else {
-	console.log('Service workers are not supported.');
+	navigator.serviceWorker
+		.register('/sw.js')
+		.then((registration) => {})
+		.catch((error) => error);
 }
